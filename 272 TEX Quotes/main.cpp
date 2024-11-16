d9b4e7e142133558e0e5ffc512bc93d7eb7f18ce
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-11-16 17:24:43 +0800

    272 TEX Quotes

--- 272 TEX Quotes/main.cpp
+++ 272 TEX Quotes/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+    string line;
+    bool status = true;
+    
+    while(getline(cin,line)) {
+
+        for(int i = 0;i < line.size();i++) {
+            if(line[i] == '"') {
+                if(status) {
+                    cout << "``";
+                    status = false;
+                } else {
+                    cout << "''";
+                    status = true;
+                }
+            } else {
+                cout << line[i];
+            }
+        }
+
+        cout << endl;
+    }
+
 }
