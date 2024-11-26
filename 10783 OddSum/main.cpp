0dbae78bfd90709af58f066cee8f9b682de80c4d
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-11-26 23:28:03 +0800

    Refactor Odd Sum solution: move variable declaration outside loop and add project layout file

--- 10783 OddSum/main.cpp
+++ 10783 OddSum/main.cpp
@@ -7,10 +7,10 @@ int main()
     int start_number,end_number;
 
     int times,result;
+    int c = 1;
     cin >> times;
 
     while(times--) {
-        int c = 1;
         cin >> start_number >> end_number;
         for(int i = start_number;i <= end_number;i++) {
             if(i % 2 == 1) {
